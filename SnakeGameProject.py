3628678a5783b9dd4fa741f0e6ff66dd6810c45d
Mikołaj Skaziak <ms310792@student.polsl.pl>
2023-12-30 02:10:27 +0100

    Update SnakeGameProject.py

--- SnakeGameProject.py
+++ SnakeGameProject.py
@@ -50,28 +50,65 @@ def next_move(snake,food):
   snake.coordinates.insert(0,(x,y))  
   square=canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOUR)  
   snake.squares.insert(0,square)
-  del snake.coordinates[-1]
-  canvas.delete(snake.squares[-1])
-  del snake.squares[-1]
-  
-  window.after(GAME_SPEED,next_move,snake,food)    
+  if x==food.coordinates[0] and y==food.coordinates[1]:
+      global score
+      score+=1      
+      label.config(text="Score:{}".format(score))
+      canvas.delete("food")
+      food=Food()
+  else:
+     del snake.coordinates[-1]
+     canvas.delete(snake.squares[-1])
+     del snake.squares[-1]
+  if check_collision(snake):
+     gameover()
+  else:
+      window.after(GAME_SPEED,next_move,snake,food)    
   
 
 def change_direction(new_direction):
-    pass
+    global direction
+    
+    if new_direction=='left':
+        if direction!='right':
+            direction =new_direction 
+            
+    elif new_direction=='right':
+        if direction!='left':
+            direction =new_direction
+            
+    elif new_direction=='up':
+        if direction!='down':
+            direction =new_direction
+            
+    elif new_direction=='down':
+        if direction!='up':
+            direction =new_direction
+       
 
-def check_collision():
-    pass        
-
+def check_collision(snake):
+    
+    x,y=snake.coordinates[0]
+    
+    if x<0 or x>= GAME_WIDTH:
+        return True 
+    
+    elif y<0 or y>=GAME_HEIGHT:
+        return True     
+    for body_part in snake.coordinates[1:]:
+        if x==body_part[0] and y== body_part[1]:
+            return True
+    return False
 def gameover():
-    pass
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2,canvas.winfo_height()/2,font=('ARIAL',70),text="GAME OVER",fill="red",tag="gameover")
 #Dostosowanie Okna
 window = Tk()
 window.title("Snake Game")
 window.resizable(False, False)
 score=0
 direction='down'
-label= Label(window,text="Score:{}".format(score), font=('consolas',40))
+label= Label(window,text="Score:{}".format(score), font=('ARIAL',40))
 label.pack()
 canvas= Canvas(window,bg=BACKGROUND_COLOUR,height=GAME_HEIGHT,width=GAME_WIDTH)
 canvas.pack()
@@ -86,8 +123,7 @@ x=int((screen_width/2)-(window_width/2))
 y=int((screen_height/2)-(window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#BINDS:
-
+#Przypisanie klawiszy:
 window.bind('<Left>',lambda event: change_direction('left'))
 window.bind('<Right>',lambda event: change_direction('right'))
 window.bind('<Down>',lambda event: change_direction('down'))
